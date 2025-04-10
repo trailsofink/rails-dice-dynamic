2ec5fccf1682bbce8f1202a1959321a8c0aa8c66
trailsofink <trailsofink@gmail.com>
2025-04-10 17:19:00 -0500

    adding missed files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
+# .config/routes.rb
 
+Rails.application.routes.draw do
+  get('/', { controller: 'pages', action: 'home' })
+  get('/dice/:num_of_dice/:num_of_sides', { controller: 'pages', action: 'roll' })
 end
